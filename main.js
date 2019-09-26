c154993e3d7aa921761e41d2fc6159549ba8a3fa
jaylanwu <jaylanwu@outlook.com>
2019-09-26 16:04:23 +0800

    封装函数

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-//初始化
-$('.images>img:nth-child(1)').addClass('current')
-    .siblings().addClass('enter')
-
-let n = 1
+let n
 let size = $('.images>img').length
+initN(1)
+
 //自动无缝轮播
 setInterval(() => {
-    $('.images>img:nth-child(' + nLoop(n, size) + ')').removeClass('current').addClass('leave')
+    makeLeave(getImg(n))
         .one('transitionend', (xxx) => {
-            $(xxx.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(xxx.currentTarget))
         })
-    $('.images>img:nth-child(' + nLoop(n + 1, size) + ')').removeClass('enter').addClass('current')
+    makeCurrent(getImg(n + 1))
     n += 1
 }, 2000);
 
+
+
+//封装函数
+//初始化
+function initN(startIndex) {
+    n = startIndex
+    $('.images>img:nth-child(' + n + ')').addClass('current')
+        .siblings().addClass('enter')
+
+}
 //取n～size的循环数
 function nLoop(n, size) {
     if (n > size) {
@@ -23,4 +31,20 @@ function nLoop(n, size) {
         }
     }
     return n
+}
+//获取img
+function getImg(n) {
+    return $('.images>img:nth-child(' + nLoop(n, size) + ')')
+}
+//切换状态
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+
+function makeEnter($node) {
+    return $node.removeClass('current leave').addClass('enter')
 }
