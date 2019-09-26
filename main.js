5c225f3618378931cadedfdc6c0b87c6b7d99f51
jaylanwu <jaylanwu@outlook.com>
2019-09-26 14:21:42 +0800

    自动轮播

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 //初始化
 $('.images>img:nth-child(1)').addClass('current')
-$('.images>img:nth-child(2)').addClass('enter')
-$('.images>img:nth-child(3)').addClass('enter')
-//初步逻辑实现
-setTimeout(() => {
-    $('.images>img:nth-child(1)').removeClass('current').addClass('leave')
-    .one('transitionend',(xxx)=>{
-        $(xxx.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images>img:nth-child(2)').removeClass('enter').addClass('current')
+    .siblings().addClass('enter')
+
+let n = 1
+let size = $('.images>img').length
+//自动无缝轮播
+setInterval(() => {
+    $('.images>img:nth-child(' + nLoop(n, size) + ')').removeClass('current').addClass('leave')
+        .one('transitionend', (xxx) => {
+            $(xxx.currentTarget).removeClass('leave').addClass('enter')
+        })
+
+    $('.images>img:nth-child(' + nLoop(n + 1, size) + ')').removeClass('enter').addClass('current')
+    n += 1
 }, 2000);
 
-setTimeout(() => {
-    $('.images>img:nth-child(2)').removeClass('current').addClass('leave')
-    .one('transitionend',(xxx)=>{
-        $(xxx.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images>img:nth-child(3)').removeClass('enter').addClass('current')
-}, 4000);
-
-setTimeout(() => {
-    $('.images>img:nth-child(3)').removeClass('current').addClass('leave')
-    .one('transitionend',(xxx)=>{
-        $(xxx.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images>img:nth-child(1)').removeClass('enter').addClass('current')
-}, 6000);
+//取n～size的循环数
+function nLoop(n, size) {
+    if (n > size) {
+        n = n % size
+        if (n == 0) {
+            n = size
+        }
+    }
+    return n
+}
